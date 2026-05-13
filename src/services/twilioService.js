76cd7984ad95451af13a56d40d9892577f04f02f
Shae Gosal <shaegosal@email.com>
2026-05-13 19:51:23 -0400

    fix twilio client init

--- src/services/twilioService.js
+++ src/services/twilioService.js
@@ -1,11 +1,11 @@
 const twilio = require('twilio');
 
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
-
 async function sendSMS(to, body) {
+  const client = twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
   try {
     const message = await client.messages.create({
       body,
